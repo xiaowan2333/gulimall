0dca9f5b15d6350084244a5948efa92884678c36
xiaowan2333 <1055709919@qq.com>
2022-07-12 14:42:35 +0800

    init

---
+++ gulimall-ware/src/test/java/com/xtest/gulimall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xtest.gulimall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
